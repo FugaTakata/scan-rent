e6b4fa6d6abac146cdc4fe688ac6b474837074ed
FugaTakata <fuga.takata.dev@gmail.com>
2023-12-19 16:35:20 +0900

    playwrightでのスクレイピング

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,7 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import {chromium} from "playwright";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +18,43 @@ export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+
+export const scrapeSuumo = onRequest(async (request, response) => {
+  const targetUrl = "SUUMOの賃貸物件の情報が載ったページのURL";
+
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  await page.goto(targetUrl);
+
+  // 物件タイトルの取得
+  const title = await page.locator(
+    "#wrapper > div.section_h1 > div.section_h1-header > h1"
+  ).textContent();
+
+  // 写真の取得
+  const imgUrls = await Promise.all(
+    (
+      await page.locator("#js-view_gallery-list li a img").all()
+    ).map((img) => img.getAttribute("src"))
+  );
+  const imgs = await Promise.all(
+    imgUrls.map(async (imgUrl) => {
+      const response = await fetch(imgUrl as string);
+      const arrayBuffer = await response.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      const contentType = response.headers.get("Content-Type");
+      const base64 = `data:${contentType};base64,${buffer.toString("base64")}`;
+
+      return base64;
+    })
+  );
+
+  await browser.close();
+
+  logger.info(title, {structuredData: true});
+  logger.info(imgs, {structuredData: true});
+  response.send(title);
+});
+

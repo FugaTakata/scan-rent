17280c21d2effe28da4d52bae66d5be4cdae4a03
FugaTakata <fuga.takata.dev@gmail.com>
2023-12-19 18:58:56 +0900

    Functions上でのスクレイピング

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,41 +20,47 @@ export const helloWorld = onRequest((request, response) => {
 });
 
 
-export const scrapeSuumo = onRequest(async (request, response) => {
-  const targetUrl = "SUUMOの賃貸物件の情報が載ったページのURL";
+export const scrapeSuumo = onRequest(
+  {timeoutSeconds: 100, memory: "1GiB"},
+  async (request, response) => {
+    // const targetUrl = "SUUMOの賃貸物件の情報が載ったページのURL";
+    const targetUrl =
+      "https://suumo.jp/chintai/jnc_000085234830/?bc=100349846901";
 
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
 
-  await page.goto(targetUrl);
+    await page.goto(targetUrl);
 
-  // 物件タイトルの取得
-  const title = await page.locator(
-    "#wrapper > div.section_h1 > div.section_h1-header > h1"
-  ).textContent();
+    // 物件タイトルの取得
+    const title = await page
+      .locator("#wrapper > div.section_h1 > div.section_h1-header > h1")
+      .textContent();
 
-  // 写真の取得
-  const imgUrls = await Promise.all(
-    (
-      await page.locator("#js-view_gallery-list li a img").all()
-    ).map((img) => img.getAttribute("src"))
-  );
-  const imgs = await Promise.all(
-    imgUrls.map(async (imgUrl) => {
-      const response = await fetch(imgUrl as string);
-      const arrayBuffer = await response.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
-      const contentType = response.headers.get("Content-Type");
-      const base64 = `data:${contentType};base64,${buffer.toString("base64")}`;
+    // 写真の取得
+    const imgUrls = await Promise.all(
+      (
+        await page.locator("#js-view_gallery-list li a img").all()
+      ).map((img) => img.getAttribute("src"))
+    );
+    const imgs = await Promise.all(
+      imgUrls.map(async (imgUrl) => {
+        const response = await fetch(imgUrl as string);
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        const contentType = response.headers.get("Content-Type");
+        const base64 = `data:${contentType};base64,${buffer.toString(
+          "base64"
+        )}`;
 
-      return base64;
-    })
-  );
+        return base64;
+      })
+    );
 
-  await browser.close();
+    await browser.close();
 
-  logger.info(title, {structuredData: true});
-  logger.info(imgs, {structuredData: true});
-  response.send(title);
-});
-
+    logger.info(title, {structuredData: true});
+    logger.info(imgs, {structuredData: true});
+    response.send(title);
+  }
+);
